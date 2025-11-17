35629a45fd9a247df589982c378c75b43c9f4861
reproducepark <reproducepark@kaist.ac.kr>
2025-11-17 13:23:17 +0900

    dreambooth complete

--- task_2_lora/scripts/train_dreambooth_lora_slipper.sh
+++ task_2_lora/scripts/train_dreambooth_lora_slipper.sh
@@ -1,17 +1,12 @@
 export MODEL_NAME="CompVis/stable-diffusion-v1-4"
 export INSTANCE_DIR="./sample_data/dreambooth-slipper"
-export OUTPUT_DIR="./runs/dreambooth_slipper_1000"
+export OUTPUT_DIR="./runs/dreambooth_slipper_500"
 export CLASS_DIR="./sample_data/dreambooth-slipper-class"
 
 accelerate launch --mixed_precision="no" train_dreambooth_lora.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
   --instance_data_dir=$INSTANCE_DIR \
   --output_dir=$OUTPUT_DIR \
-  --class_data_dir=$CLASS_DIR \
-  --with_prior_preservation \
-  --class_prompt="a photo of a slipper" \
-  --num_class_images=100 \
-  --prior_loss_weight=1.0 \
   --instance_prompt="a photo of a sks slipper" \
   --resolution=512 \
   --train_batch_size=1 \
@@ -20,7 +15,7 @@ accelerate launch --mixed_precision="no" train_dreambooth_lora.py \
   --learning_rate=1e-4 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
-  --max_train_steps=1000 \
+  --max_train_steps=500 \
   --validation_prompt="A photo of sks slipper in a jungle" \
   --validation_epochs=50 \
   --checkpoints_total_limit 2 \
